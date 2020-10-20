362c2bd0f9e35a68695de1e03c5fa8194686649a
muhammad-usama-aleem <you@example.com>
2020-10-20 20:24:42 +0500

    adding database for project

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = knex = require('knex');
+
+knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+});
 
 const app = express();
 
